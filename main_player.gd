2d0c65624a65be6435064cdf7efd7ae499c567f7
mpallansch <matthew.pallansch@gmail.com>
2024-06-18 00:05:35 -0400

    Improves Movement Animation

--- main_player.gd
+++ main_player.gd
@@ -2,44 +2,57 @@ extends CharacterBody2D
 
 var paused
 
-
 var health = 40
 var health_max = 80
 var attack_power = 10
 var defense = 4
+
+var last_direction = "up"
+var movement_animation_mapping = {
+	"up": {
+		"idle": "IdleUp",
+		"walk": "WalkUp",
+		"punch": "PunchUp",
+		"active": false
+	},
+	"down": {
+		"idle": "IdleDown",
+		"walk": "WalkDown",
+		"punch": "PunchDown",
+		"active": false
+	},
+	"left": {
+		"idle": "IdleLeft",
+		"walk": "WalkLeft",
+		"punch": "PunchLeft",
+		"active": false
+	},
+	"right": {
+		"idle": "IdleRight",
+		"walk": "WalkRight",
+		"punch": "PunchRight",
+		"active": false
+	},
+}
 
 # UI nodes
 @onready var pause_screen =  %Pause_Menu
 # assuming the character by default looks to the right
 
 
-
-
 func _input(event):
-	#interact with world         
-		#if event.is_action_pressed("ui_interact"):
-			#var target = ray_cast.get_collider()
-		#if target != null:
-			#if target.is_in_group("NPC"):
-				# Talk to NPC
-			   # target.dialog()
-			#return  
-			
-# ---------------- Pause Menu -------------------------------------------
-	 
-#show pause menu
-					if !pause_screen.visible:
-						if event.is_action_pressed("Pause"):
-				#pause game     s
-							get_tree().paused = true
-				#show pause screen popup
-							%Pause_Menu.visible = true
-				#stops movement processing 
-							set_physics_process(false)
-				#set pauses state to be true
-							paused = true
-							#resume game
-				
+	if !pause_screen.visible:
+		if event.is_action_pressed("Pause"):
+			get_tree().paused = true
+			#show pause screen popup
+			%Pause_Menu.visible = true
+			#stops movement processing 
+			set_physics_process(false)
+			#set pauses state to be true
+			paused = true
+			#resume game
+
+
 func _on_resume_pressed():
 		#hide pause menu
 	pause_screen.visible = false
@@ -49,70 +62,30 @@ func _on_resume_pressed():
 		#accept movement and input
 	set_process_input(true)
 	set_physics_process(true)  
-	
-#func _on_quit_pressed():
-#		Global.change_scene("res://Scenes/main_scene.tscn")
-#		get_tree().paused = false
 
-#		Global.change_scene("res://Scenes/MainScene.tscn")
-#		get_tree().paused = false
-#		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
+
 func _process(delta):
-	#Movement when direction button pressed
-	if Input.is_action_just_pressed("down"):
-		$AnimationPlayer.play("WalkDown");
-	if Input.is_action_just_pressed("right"):
-		$AnimationPlayer.play("WalkRight");
-	if Input.is_action_just_pressed("ui_left"):
-		$AnimationPlayer.play("WalkLeft");
-	if Input.is_action_just_pressed("ui_up"):
-		$AnimationPlayer.play("WalkUp");
+	var moving = false
+	for action in movement_animation_mapping:
+		var action_obj = movement_animation_mapping[action]
+		if Input.is_action_just_pressed(action):
+			action_obj.active = true
+			last_direction = action
+		if Input.is_action_just_released(action):
+			action_obj.active = false
+			last_direction = action
+		if action_obj.active:
+			moving = true
+			last_direction = action
 	
-	#Movement when direction button released
-	if Input.is_action_just_released("down"):
-		$AnimationPlayer.play("IdleDown")
-	if Input.is_action_just_released("right"):
-		$AnimationPlayer.play("IdleRight")
-	if Input.is_action_just_released("left"):
-		$AnimationPlayer.play("IdleLeft")
-	if Input.is_action_just_released("up"):
-		$AnimationPlayer.play("IdleUp")
 	
-	if ($AnimationPlayer.current_animation == "WalkDown" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchDown")
-		
-	if ($AnimationPlayer.current_animation == "IdleDown" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchDown")
-	if Input.is_action_just_released("Punch"):
-		$AnimationPlayer.stop()
-	
-	if ($AnimationPlayer.current_animation == "WalkLeft" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchLeft")
-		
-	if ($AnimationPlayer.current_animation == "IdleLeft" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchLeft")
-		
-	if ($AnimationPlayer.current_animation == "WalkRight" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchRight")
-		
-	if ($AnimationPlayer.current_animation == "IdleRight" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchRight")
-		
-	if ($AnimationPlayer.current_animation == "WalkUp" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchUp")
-		
-	if ($AnimationPlayer.current_animation == "IdleUp" && 
-	Input.is_action_just_pressed("Punch")):
-		$AnimationPlayer.play("PunchUp")
-		print($AnimationPlayer.current_animation)
+	var action_obj = movement_animation_mapping[last_direction]
+	if Input.is_action_just_pressed("Punch"):
+		$AnimationPlayer.play(action_obj.punch)
+	elif moving:
+		$AnimationPlayer.play(action_obj.walk)
+	else:
+		$AnimationPlayer.play(action_obj.idle)
 		
 	
 	
